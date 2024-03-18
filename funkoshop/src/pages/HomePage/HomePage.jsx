b84e7fa28967f1a672d855a5bafc03785b3c7bb4
PabloMEcheverria <pablomartinecheverria@gmail.com>
2024-03-17 23:59:58 -0300

    Update

--- funkoshop/src/pages/HomePage/HomePage.jsx
+++ funkoshop/src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import './HomePage.css';
 import presentationArr from '../../data/licPresentation.js';
 import LicensePresentation from '../../components/LicensePresentation.jsx';
 import Carousel from '../../components/Carousel.jsx';
+import { Link } from 'react-router-dom';
 
 export default function HomePage() {
     return (
@@ -10,7 +11,7 @@ export default function HomePage() {
                 <div className='newProducts_content'>
                     <h4 className='newProducts_title'>Nuevos Ingresos</h4>
                     <p className='newProducts_description'>Descubrí el próximo Funko Pop de tu colección</p>
-                    <button className='newProducts_button'>Shop</button>
+                    <Link to="/shop" className='newProducts_button'>Shop</Link>
                 </div>
             </section>
             {presentationArr.map( (license, i) => 
