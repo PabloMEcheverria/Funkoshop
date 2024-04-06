60af58541e8cba25b5f9a72a25fcdf29158fc496
Pablo Echeverria <pablomartinecheverria@gmail.com>
2024-04-05 22:10:54 -0300

    Working on checkboxs styles in filter form.

--- funkoshop/src/pages/Shop/ShopPage.jsx
+++ funkoshop/src/pages/Shop/ShopPage.jsx
@@ -2,8 +2,8 @@ import { uniqueProductsArr } from "../../data/products";
 //import { Link, Outlet } from "react-router-dom";
 import { useState } from "react";
 import "./ShopPage.css";
-import SelectArrowDown from '../../components/svgComponents/SelectArrowDown.jsx';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 export default function ShopPage() {
     /*const linkProductArr = uniqueProductsArr.map((product) => (
         <Link key={product.id} to={`/shop/${product.id}`} style={{display: "block", marginBottom: "5px", marginLeft: "5px"}}>
@@ -163,60 +163,63 @@ export default function ShopPage() {
                     <fieldset>
                         <legend>precio</legend>
                         <div>
-                            <label for="min">min</label>
+                            <label htmlFor="min">min</label>
                             <input type="number" name="price" id="min" placeholder="0" onChange={handleInputChange}/>
                             <p>-</p>
-                            <label for="max">max</label>
+                            <label htmlFor="max">max</label>
                             <input type="number" name="price" id="max" placeholder="0" onChange={handleInputChange} />
                         </div>
                     </fieldset>
 
                     <fieldset>
                         <legend>filtrar</legend>
+                        
+                        <div>
+                            <label htmlFor="filterByNew">
+                                <input 
+                                    type="checkbox" 
+                                    id="filterByNew" 
+                                    name="filterByNew" 
+                                    value="isNew"
+                                    checked={filterData.filterByNew}
+                                    onChange={handleInputChange} />
+                                nuevos
+                            </label>
 
-                        <label htmlFor="filterByNew">
-                            <input 
+                            <label htmlFor="filterByOffer">
+                                <input 
+                                    type="checkbox" 
+                                    id="filterByOffer" 
+                                    name="filterByOffer" 
+                                    value="isOffer"
+                                    checked={filterData.filterByOffer}
+                                    onChange={handleInputChange} />
+                                ofertas
+                            </label>
+
+                            <label htmlFor="filterBySpecialEdition">
+                                <input 
+                                    type="checkbox" 
+                                    id="filterBySpecialEdition" 
+                                    name="filterBySpecialEdition" 
+                                    value="isSpecialEdition"
+                                    checked={filterData.filterBySpecialEdition}
+                                    onChange={handleInputChange} />
+                                edición especial
+                            </label>
+
+                            <label htmlFor="filterByFavorites">
+                                <FontAwesomeIcon icon={faCheck} />
+                                <input 
                                 type="checkbox" 
-                                id="filterByNew" 
-                                name="filterByNew" 
-                                value="isNew"
-                                checked={filterData.filterByNew}
+                                id="filterByFavorites" 
+                                name="filterByFavorites" 
+                                value="isFavorite"
+                                checked={filterData.filterByFavorites}
                                 onChange={handleInputChange} />
-                            nuevos
-                        </label>
-
-                        <label htmlFor="filterByOffer">
-                            <input 
-                                type="checkbox" 
-                                id="filterByOffer" 
-                                name="filterByOffer" 
-                                value="isOffer"
-                                checked={filterData.filterByOffer}
-                                onChange={handleInputChange} />
-                            ofertas
-                        </label>
-
-                        <label htmlFor="filterBySpecialEdition">
-                            <input 
-                                type="checkbox" 
-                                id="filterBySpecialEdition" 
-                                name="filterBySpecialEdition" 
-                                value="isSpecialEdition"
-                                checked={filterData.filterBySpecialEdition}
-                                onChange={handleInputChange} />
-                            edición especial
-                        </label>
-
-                        <label htmlFor="filterByFavorites">
-                            <input 
-                            type="checkbox" 
-                            id="filterByFavorites" 
-                            name="filterByFavorites" 
-                            value="isFavorite"
-                            checked={filterData.filterByFavorites}
-                            onChange={handleInputChange} />
-                            favoritos
-                        </label>
+                                favoritos
+                            </label>
+                        </div>
                     </fieldset>
                 </form>
             </aside>
