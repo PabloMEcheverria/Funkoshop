ba71416f2e2903f41ce63c0c48f0676a21827f26
PabloMEcheverria <pablomartinecheverria@gmail.com>
2024-04-29 01:03:05 -0300

    Fixing PaginationButton component, disabling ellipsis buttons, inserting selected classes in current position pagination's button.

--- funkoshop/src/components/Pagination.jsx
+++ funkoshop/src/components/Pagination.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../assets/css/Pagination.css";
 import PaginationButton from "./PaginationButton";
 
@@ -14,10 +14,6 @@ export default function Pagination({ displayProductArr, setProductArr, paginatio
         console.log(pagination(displayProductArr));
         setPaginationData(pagination(displayProductArr));
     }, [displayProductArr, setPaginationData]);
-
-    function moveTo(e) {
-        console.log(e.target);
-    }
 
     function pagination(productArr) {
         let newPaginationData = {...paginationData};
@@ -37,50 +33,20 @@ export default function Pagination({ displayProductArr, setProductArr, paginatio
         let paginationLinkArr = newProductArr.map((productArr, i, arr) => {
             let positionInPagination = paginationData.positionInPagination;
             if (arr.length <= 7) {
-                return (
-                    <li>
-                        <button className="pagination__button" onClick={moveTo}>
-                            {i + 1}
-                        </button>
-                    </li>
-                )
+                return (<PaginationButton buttonData={buttonData} index={i + 1} />)
             } else if (arr.length > 7) {
                 if (positionInPagination <= 2) {
                     if (i + 1 <= 3 || i + 1 === arr.length) {
-                        return (
-                            <li>
-                                <button className="pagination__button" onClick={moveTo}>
-                                    {i + 1}
-                                </button>
-                            </li>
-                        )
+                        return (<PaginationButton buttonData={buttonData} index={i + 1} />)
                     } else if (i + 1 === 4) {
-                        return (
-                            <li>
-                                <button className="pagination__button" onClick={moveTo} isEllipsis={true}>
-                                    ...
-                                </button>
-                            </li>
-                        )
+                        return (<PaginationButton buttonData={buttonData} isEllipsis={true} />)
                     }
                 }
                 if (positionInPagination === 3) {
                     if (i + 1 <= 4 || i + 1 === arr.length) {
-                        return (
-                            <li>
-                                <button className="pagination__button" onClick={moveTo}>
-                                    {i + 1}
-                                </button>
-                            </li>
-                        )
+                        return (<PaginationButton buttonData={buttonData} index={i + 1} />)
                     } else if (i + 1 === 5) {
-                        return (
-                            <li>
-                                <button className="pagination__button" isEllipsis={true} onClick={moveTo}>
-                                    ...
-                                </button>
-                            </li>
-                        )
+                        return (<PaginationButton buttonData={buttonData} isEllipsis={true} />)
                     }
                 }
                 if (positionInPagination > 3 && positionInPagination <= arr.length - 2) {
@@ -88,75 +54,47 @@ export default function Pagination({ displayProductArr, setProductArr, paginatio
                         i + 1 === arr.length || (
                         i + 1 >= positionInPagination - 1 && i + 1 <= positionInPagination + 1
                     )) {
-                        return (
-                            <li>
-                                <button className="pagination__button" onClick={moveTo}>
-                                    {i + 1}
-                                </button>
-                            </li>
-                        )
+                        return (<PaginationButton buttonData={buttonData} index={i + 1} />)
                     } else if (i + 1 === 2 || i + 1 === arr.length - 1) {
-                        return (
-                            <li>
-                                <button className="pagination__button" onClick={moveTo} isEllipsis={true}>
-                                    ...
-                                </button>
-                            </li>
-                        )
+                        return (<PaginationButton buttonData={buttonData} isEllipsis={true} />)
                     }
                 }
                 if (positionInPagination === arr.length - 2) {
                     if (i + 1 === 1 || i + 1 >= arr.legend - 3) {
-                        return (
-                            <li>
-                                <button className="pagination__button" onClick={moveTo}>
-                                    {i + 1}
-                                </button>
-                            </li>
-                        )
+                        return (<PaginationButton buttonData={buttonData} index={i + 1} />)
                     } else if (i + 1 === 2) {
-                        <li>
-                            <button className="pagination__button" onClick={moveTo} isEllipsis={true}>
-                                ...
-                            </button>
-                        </li>
+                        return (<PaginationButton buttonData={buttonData} isEllipsis={true} />)
                     }
                 }
                 if (positionInPagination >= arr.length - 1) {
                     if (i + 1 === 1 || i + 1 >= arr.length - 2) {
-                        return (
-                            <li>
-                                <button className="pagination__button" onClick={moveTo}>
-                                    {i + 1}
-                                </button>
-                            </li>
-                        )
+                        return (<PaginationButton buttonData={buttonData} index={i + 1} />)
                     } else if (i + 1 === 2) {
-                        return (
-                            <li>
-                                <button className="pagination__button" onClick={moveTo} isEllipsis={true}>
-                                    ...
-                                </button>
-                            </li>
-                        )
+                        return (<PaginationButton buttonData={buttonData} isEllipsis={true} />)
                     }
                 }
             }
         });
         paginationLinkArr = paginationLinkArr.filter(element => element !== undefined);
-        paginationLinkArr.unshift(<li><button className="pagination__button" onClick={moveTo}>prev</button></li>);
-        paginationLinkArr.push(<li><button className="pagination__button" onClick={moveTo}>next</button></li>);
+        paginationLinkArr.unshift(<PaginationButton buttonData={buttonData} isPrev={true} />);
+        paginationLinkArr.push(<PaginationButton buttonData={buttonData} isNext={true} />);
         newPaginationData.paginationList = <ul>{paginationLinkArr.map((link, i, arr) => {
             if (i === 0) {
-                return (<li key={"prev"} id={"prev"} className="pagination__item">{link.props.children}</li>)
+                return (<li key={"prev"} id={"prev"} className="pagination__item">{link}</li>)
             } else if (i === arr.length - 1) {
-                return (<li key={"next"} id={"next"} className="pagination__item">{link.props.children}</li>)
+                return (<li key={"next"} id={"next"} className="pagination__item">{link}</li>)
             } else {
-                return (<li key={i} id={i} className="pagination__item">{link.props.children}</li>)       
+                console.log(link.props.index);
+                if (link.props.index === paginationData.positionInPagination) {
+                    return (<li key={i} id={i} className="pagination__item--selected">{link}</li>)
+                } else {
+                    return (<li key={i} id={i} className="pagination__item">{link}</li>)
+                }
             }
         })}</ul>;
-        //disable ellipsis button.
-        //select "selected" number's button.
+        console.log(newPaginationData.paginationList.props.children);
+        //disable ellipsis button. CHECKED.
+        //select "selected" number's button. CHECKED.
         //disable prev when "1" button is selected.
         //disable next when last number button is selected.
         //disable prev and next when there is just one numbered button.
--- funkoshop/src/components/PaginationButton.jsx
+++ funkoshop/src/components/PaginationButton.jsx
@@ -1,9 +1,23 @@
 import "../assets/css/PaginationButton.css";
 
-export default function PaginationButton({ buttonData, onClickFunction, index = "...", disabledValue = false }) {
+export default function PaginationButton(
+    { buttonData, index = "...", isPrev = false, isNext = false, isEllipsis = false, disabledValue = false }) {
+    const {moveTo} = buttonData;
+    let content;
+    if (isPrev) {
+        content = "prev";
+    } else if (isNext) {
+        content = "next";
+    } else if (isEllipsis) {
+        content = "..."
+    } else {
+        content = index;
+    }
     return (
-        <button className="pagination__button" onClick={onClickFunction} disabled={disabledValue} >
-            {index !== "..." ? index + 1 : index}
+        <button className={isEllipsis ? "pagination__button--ellipsis": "pagination__button"} 
+                onClick={moveTo} 
+                disabled={isEllipsis ? true : disabledValue} >
+            {content}
         </button>
     )
 }
