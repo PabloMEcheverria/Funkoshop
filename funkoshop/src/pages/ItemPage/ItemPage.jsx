02a4cd12f4e0524b5ef9d5006882fa396f253fb1
PabloMEcheverria <pablomartinecheverria@gmail.com>
2024-06-08 00:15:40 -0300

    Start to work in ItemPage component.

--- funkoshop/src/pages/ItemPage/ItemPage.jsx
+++ funkoshop/src/pages/ItemPage/ItemPage.jsx
@@ -1,12 +1,19 @@
 import "./ItemPage.css";
 import { useParams } from "react-router-dom";
+import productsArr from "../../data/products";
+import { useState } from "react";
 
 export default function ItemPage() {
     const params = useParams();
-    console.log(params);
+    const itemId = params.itemId;
+    const prod = productsArr.filter(product => parseInt(itemId) === product.id)[0];
+    const [product, setProduct] = useState(prod);
+    console.log(product);
    return (
     <>
-        <p style={{padding: "5px", backgroundColor: "coral", display: "inline-block"}}>Item page {params.itemId}</p>
+        <section className="productDetails">
+            <img src={product.frontImg} alt={"Imagen de funko pop de " + product.nameProduct} />
+        </section>
     </>
    )
 }
