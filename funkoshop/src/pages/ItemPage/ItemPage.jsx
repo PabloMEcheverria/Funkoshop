0d05cf7625a33fd1e8140b9d58dc2df080f0d499
PabloMEcheverria <pablomartinecheverria@gmail.com>
2024-06-11 00:58:26 -0300

    Working on product's details.

--- funkoshop/src/pages/ItemPage/ItemPage.jsx
+++ funkoshop/src/pages/ItemPage/ItemPage.jsx
@@ -13,6 +13,21 @@ export default function ItemPage() {
     <>
         <section className="productDetails">
             <img src={product.frontImg} alt={"Imagen de funko pop de " + product.nameProduct} />
+            <div>
+                <p className="license">{product.license}</p>
+                <h2 className="nameProduct">{product.nameProduct}</h2>
+                <p>{product.price}</p>
+                <input type="number" />
+                <button>+</button>
+                <button>-</button>
+                <button>Agregar al Carrito</button>
+                <div>
+                    <a href="#">Ver métodos de pago</a>
+                    <p>- {  product.paymentMethods.length === 1 ? 
+                            "Efectivo o débito automático" : 
+                            `${product.paymentMethods[product.paymentMethods.length - 1]} CUOTAS SIN INTERÉS`}</p>
+                </div>
+            </div>
         </section>
     </>
    )
