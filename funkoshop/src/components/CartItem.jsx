7440931fc673919888d926f136ce20c15acf1a80
PabloMEcheverria <pablomartinecheverria@gmail.com>
2024-08-09 01:32:34 -0300

    Adding useState and useEffect hooks and still working on styles in CartItem component.

--- funkoshop/src/components/CartItem.jsx
+++ funkoshop/src/components/CartItem.jsx
@@ -2,21 +2,30 @@ import "../assets/css/CartItem.css";
 import ItemShopMinus from "./svgComponents/ItemShopMinus";
 import ItemShopPlus from "./svgComponents/ItemShopPlus";
 import CancelIcon from "./svgComponents/CancelIcon";
+import { useEffect, useState } from "react";
 
 export default function CartItem({ name, quantity, itemsInCart, setItemsInCart, productsStock, setProductsStock }) {
-    const item = itemsInCart.find(currentValue => currentValue.nameProduct === name);
+    const [cartProduct, setCartProduct] = useState({
+        item: itemsInCart.find(currentValue => currentValue.nameProduct === name), 
+        totalPrice: "$ "
+    });
+    useEffect(() => {
+        let total = cartProduct.item.price * quantity;
+        total = Math.round(total * 100) / 100;
+        setCartProduct({...cartProduct, totalPrice: `$ ${total}`});
+    }, [quantity, itemsInCart, productsStock, cartProduct]);
     return (
         <>
             <tr className="cart-item">
                 <td className="cart-item__details">
                     <div className="cart-item__info">
                         <div className="cart-item__image">
-                            <img src={item.frontImg} alt={item.description} />
+                            <img src={cartProduct.item.frontImg} alt={cartProduct.item.description} />
                         </div>
                         <div className="cart-item__description">
-                            <h4 className="cart-item__name">{item.nameProduct}</h4>
-                            <p className="cart-item__license">{item.license}</p>
-                            <p className="cart-item__price">${item.price}</p>
+                            <h4 className="cart-item__name">{cartProduct.item.nameProduct}</h4>
+                            <p className="cart-item__license">{cartProduct.item.license}</p>
+                            <p className="cart-item__price">precio: $ {cartProduct.item.price}</p>
                         </div>
                     </div>
                     <div className="cart-item__controls">
@@ -31,7 +40,7 @@ export default function CartItem({ name, quantity, itemsInCart, setItemsInCart, 
                         </div>
                     </div>
                     <div className="cart-item__summary">
-                        <p className="cart-item__total-price">$ lorem,ipsum</p> {/*Sum of price of all products that are the same item.*/}
+                        <p className="cart-item__total-price">{cartProduct.totalPrice}</p> {/*Sum of price of all products that are the same item.*/}
                     </div>
                     <div>
                         <button className="cart-item__remove-button-wrapper">
