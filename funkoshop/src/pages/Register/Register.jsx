57c66f6e49a4a31109b63d639e3bd37db8f609c0
PabloMEcheverria <pablomartinecheverria@gmail.com>
2025-02-25 01:46:10 -0300

    Styling register page.

--- funkoshop/src/pages/Register/Register.jsx
+++ funkoshop/src/pages/Register/Register.jsx
@@ -41,37 +41,39 @@ export default function Register() {
         <main className="signup">
           <h1 className="signup__title">Crea tu cuenta</h1>
           <p className="signup__description">Completa el formulario para ser parte del mundo de los Funkos</p>
-          <form className="signup__form">
-            <section className="signup__section">
-              <label className="signup__label" htmlFor="name">Nombre:</label>
-              <input className="signup__input" type="text" id="name" name="name" placeholder="John" onChange={e => setName(e.target.value)} />
-            </section>
-            <section className="signup__section">
-              <label className="signup__label" htmlFor="lastName">Apellido:</label>
-              <input className="signup__input" type="text" id="lastName" name="lastName" placeholder="Doe" onChange={e => setLastName(e.target.value)} />
-            </section>
-            <section className="signup__section">
-              <label className="signup__label" htmlFor="email">Email:</label>
-              <input className="signup__input" type="email" id="email" name="email" placeholder="johndoe@correo.com" onChange={e => setEmail(e.target.value)} />
-            </section>
-            <section className="signup__section">
-              <label className="signup__label" htmlFor="password">Contraseña:</label>
-              <input className="signup__input" type="password" id="password" name="password" placeholder="•••••••••" onChange={e => setPassword(e.target.value)} />
-            </section>
-            {/*<section className="signup__section">
-              <label className="signup__label" htmlFor="confirmPassword">Repita Contraseña:</label>
-              <input className="signup__input" type="password" id="confirmPassword" name="confirmPassword" placeholder="•••••••••" />
-            </section>*/}
-            <section className="signup__section signup__section--submit">
-              <button className="signup__button" type="submit" onClick={(e) => handleClick(e)}>Registrar</button>
-              <div className="signup__terms">
-                <input className="signup__checkbox" type="checkbox" />
-                <p className="signup__terms-text">
-                  Acepto <a href="/Funkoshop/terms" className="signup__terms-link">Términos y Condiciones</a>
-                </p>
-              </div>
-            </section>
-          </form>
+          <section className="signup__container">
+            <form className="signup__form">
+              <section className="signup__section">
+                <label className="signup__label" htmlFor="name">Nombre:</label>
+                <input className="signup__input" type="text" id="name" name="name" placeholder="John" onChange={e => setName(e.target.value)} />
+              </section>
+              <section className="signup__section">
+                <label className="signup__label" htmlFor="lastName">Apellido:</label>
+                <input className="signup__input" type="text" id="lastName" name="lastName" placeholder="Doe" onChange={e => setLastName(e.target.value)} />
+              </section>
+              <section className="signup__section">
+                <label className="signup__label" htmlFor="email">Email:</label>
+                <input className="signup__input" type="email" id="email" name="email" placeholder="johndoe@correo.com" onChange={e => setEmail(e.target.value)} />
+              </section>
+              <section className="signup__section">
+                <label className="signup__label" htmlFor="password">Contraseña:</label>
+                <input className="signup__input" type="password" id="password" name="password" placeholder="•••••••••" onChange={e => setPassword(e.target.value)} />
+              </section>
+              <section className="signup__section">
+                <label className="signup__label" htmlFor="confirmPassword">Repita Contraseña:</label>
+                <input className="signup__input" type="password" id="confirmPassword" name="confirmPassword" placeholder="•••••••••" />
+              </section>
+              <section className="signup__section signup__section--submit">
+                <button className="signup__button" type="submit" onClick={(e) => handleClick(e)}>Registrar</button>
+                <div className="signup__terms">
+                  <input className="signup__checkbox" type="checkbox" />
+                  <p className="signup__terms-text">
+                    Acepto <a href="/Funkoshop/terms" className="signup__terms-link">Términos y Condiciones</a>
+                  </p>
+                </div>
+              </section>
+            </form>
+          </section>
         </main>
     )
 }
