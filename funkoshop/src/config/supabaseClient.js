e92e5a0ee317af6fc1c92941add73ce5566fba79
PabloMEcheverria <pablomartinecheverria@gmail.com>
2026-01-12 00:05:50 -0300

    Setting env variables.

--- funkoshop/src/config/supabaseClient.js
+++ funkoshop/src/config/supabaseClient.js
@@ -1,10 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://fiiwxofocddfgwpisvgu.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpaXd4b2ZvY2RkZmd3cGlzdmd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkxMjk0MTIsImV4cCI6MjA0NDcwNTQxMn0.z43UYoRUoXduLr6Trn0Cw-_lB2jIwcVrI7acKe1519o"
-
-//const supabaseUrl = process.env.REACT_APP_SUPAABSE_URL
-//const supabaseKey = process.env.REACT_APP_ANON_KEY
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_ANON_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
